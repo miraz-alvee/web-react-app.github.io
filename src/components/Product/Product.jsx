debc2cb6ace680dad7785ec2204e4ad65df6fcc8
Miraz Alvee <mirazalvee98@gmail.com>
2023-03-27 15:53:44 +0600

    add all cart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,14 +3,19 @@ import './Product.css';
 
 const Product = (props) => {
     console.log(props.product);
-    const {img, name, seller, price, quantity} = props.product;
+    const {img, name, seller, price, ratings, quantity} = props.product;
 
 
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h4>{name}</h4>
-            
+            <div className='product-info'>
+                <h4 className='product-name'>{name}</h4>
+                <p>price: {price}$</p>
+                <p>Manufracturer: {seller}</p>
+                <p>Rating: {ratings} stars</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
